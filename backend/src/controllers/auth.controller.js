2a60b8593f467d05665e387458e272340599fddd
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-25 10:32:49 +0530

    implement login user logic

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -9,13 +9,13 @@ export const signUp = async (req, res) => {
     if (!username || !email || !password) {
       return res
         .status(400)
-        .json({ success: false, message: "All fields required!!!" });
+        .json({ success: false, message: "All fields required!" });
     }
 
     if (password.length < 6) {
       return res.status(400).json({
         success: false,
-        message: "Password must be atleast 6 characters!!!",
+        message: "Password must be atleast 6 characters!",
       });
     }
 
@@ -26,7 +26,7 @@ export const signUp = async (req, res) => {
         success: false,
         message:
           existUser.username === username
-            ? "Username is already taken!!!"
+            ? "Username is already taken!"
             : "Email is already registered!",
       });
     }
@@ -51,8 +51,56 @@ export const signUp = async (req, res) => {
       .status(201)
       .json({ success: true, message: "User registered successfully.", token });
   } catch (error) {
-    return res
-      .status(500)
-      .json({ success: false, message: "Error in registering user!!!" });
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong while registering user!",
+    });
+  }
+};
+
+export const signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required!" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid credentials!" });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid credentials!" });
+    }
+
+    const token = generateToken(user._id);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 2 * 24 * 60 * 60 * 1000,
+      sameSite: "none",
+      secure: false,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "User logged in successfully.",
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong while logging in!",
+    });
   }
 };
