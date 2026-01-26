94563b46062b23e267258d5fdad1eac321d901c9
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-26 08:48:47 +0530

    create login page

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const LogIn = () => {
-  return <div>LogIn</div>;
+  const [showPassword, setShowPassword] = useState("false");
+  const navigate = useNavigate();
+  return (
+    <div className="flex items-center justify-center w-full h-screen bg-slate-200">
+      <div className="w-full max-w-120 bg-white rounded-[20px] shadow-gray-400 shadow-lg flex flex-col gap-7.5 overflow-hidden">
+        <div className="w-full h-50 bg-[#33826a] rounded-b-[30%] shadow-gray-400 shadow-lg flex items-center justify-center">
+          <h1 className="text-[30px] font-bold text-[#d9f5c0]">
+            Login to <span className="text-white">Our Linkify</span>
+          </h1>
+        </div>
+
+        <form className="flex flex-col items-center w-full gap-5 mb-8">
+          <input
+            type="email"
+            placeholder="Email"
+            className="w-[90%] h-12.5 outline-none border border-[#33826a] px-5 py-2.5 bg-white rounded-lg shadow-gray-200 shadow-lg text-gray-700 text-[19px]"
+          />
+          <div className="w-[90%] h-12.5 border border-[#33826a] shadow-gray-200 shadow-lg rounded-lg overflow-hidden relative">
+            <input
+              type={showPassword ? "password" : "text"}
+              placeholder="Password"
+              className="w-full h-full outline-none px-5 py-2.5 bg-white text-gray-700 text-[19px]"
+            />
+            <span
+              className="absolute top-2.5 right-5 text-[16px] text-[#33826a] font-semibold cursor-pointer"
+              onClick={() => setShowPassword((prev) => !prev)}
+            >
+              {showPassword ? "show" : "hide"}
+            </span>
+          </div>
+          <button
+            type="submit"
+            className="px-5 py-2.5 rounded-2xl bg-[#33826a] shadow-gray-200 shadow-md text-[19px] w-50 mt-5 text-white font-semibold hover:bg-[#5ba58f] cursor-pointer"
+          >
+            Login
+          </button>
+
+          <p className="cursor-pointer" onClick={() => navigate("/signup")}>
+            Don't have an account ?{" "}
+            <span className="text-[#33826a] font-bold">SignUp</span>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
